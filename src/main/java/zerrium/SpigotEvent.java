c6c4385e6d77cf27a5cbd155ce142e7abf138dd6
BuildTools <unconfigured@null.spigotmc.org>
2020-08-08 18:18:04 +0700

    Added MySQL (not done)

--- src/main/java/zerrium/SpigotEvent.java
+++ src/main/java/zerrium/SpigotEvent.java
@@ -3,34 +3,84 @@ package zerrium;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
-import org.bukkit.Statistic;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.UUID;
 
 public class SpigotEvent extends JavaPlugin{
     public static FileConfiguration fc;
+    protected static Connection connection;
+
     @Override
     public void onEnable() {
         System.out.println(ChatColor.YELLOW+"[Stat2Discord] v0.1 by zerrium");
-        System.out.println(ChatColor.YELLOW+"[Stat2Discord] Getting player lists...");
-        this.saveDefaultConfig();
+        System.out.println(ChatColor.YELLOW+"[Stat2Discord] Connecting to MySQL database...");
+        this.saveDefaultConfig(); //get config file
         fc = this.getConfig();
-        Discord.zplayer = new ArrayList<>();
-        int counter = 0;
-        for(OfflinePlayer i: Bukkit.getOfflinePlayers()){
-            if(i.hasPlayedBefore()) {
-                counter++;
-                UUID uuid = i.getUniqueId();
-                String name = i.getName();
-                System.out.println(ChatColor.YELLOW + "[Stat2Discord]" + ChatColor.RESET + " Found player with uuid of " + uuid.toString() + " associates with " + name);
-                Discord.zplayer.add(new ZPlayer(uuid, name));
-                int a = i.getStatistic(Statistic.AVIATE_ONE_CM);
-            }
+        //MySQL connect
+        try{
+            openConnection();
+        } catch (SQLException throwables) {
+            System.out.println(ChatColor.YELLOW+"[Stat2Discord]"+ChatColor.RED+" Unable to connect to database:");
+            throwables.printStackTrace();
+            Bukkit.getPluginManager().disablePlugin(this);
+        } catch (ClassNotFoundException e) {
+            System.out.println(ChatColor.YELLOW+"[Stat2Discord]"+ChatColor.RED+" Unable to connect to database:");
+            e.printStackTrace();
+            Bukkit.getPluginManager().disablePlugin(this);
         }
+        //database query
+        try {
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery("show tables");
+            if(!rs.next()){
+                st.executeUpdate("create table player(" +
+                        "    uuid text not null," +
+                        "    name text not null," +
+                        "    primary key(uuid));" +
+                        "create table stats(" +
+                        "    uuid text not null," +
+                        "    stat text not null," +
+                        "    val bigint(19) not null," +
+                        "    foreign key(uuid) references player(uuid);");
+            }
+            rs = st.executeQuery("select * from player;");
+            System.out.println(ChatColor.YELLOW+"[Stat2Discord] Getting player list from database...");
+            int counter = 0;
+            if(!rs.next()){
+                System.out.println(ChatColor.YELLOW+"[Stat2Discord] Found nothing in database. Grabbing player lists from world save...");
+                for(OfflinePlayer i: Bukkit.getOfflinePlayers()){
+                    if(i.hasPlayedBefore()) {
+                        counter++;
+                        UUID uuid = i.getUniqueId();
+                        String name = i.getName();
+                        System.out.println(ChatColor.YELLOW + "[Stat2Discord]" + ChatColor.RESET + " Found player with uuid of " + uuid.toString() + " associates with " + name);
+                        Discord.zplayer.add(new ZPlayer(uuid, name));
+                        PreparedStatement ps = connection.prepareStatement("insert into player(uuid,name) values (?,?)");
+                        ps.setString(1, uuid.toString());
+                        ps.setString(2, name);
+                        ps.executeUpdate();
+                    }
+                }
+            }else{
+                while(rs.next()){
+                    counter++;
+                    Discord.zplayer.add(new ZPlayer(UUID.fromString(rs.getString("uuid")), rs.getString("name")));
+                }
+            }
+            System.out.println(ChatColor.YELLOW+"[Stat2Discord] Found statistic data of "+ counter +" players.");
+            rs.close();
+        } catch (SQLException throwables) {
+            System.out.println(ChatColor.YELLOW+"[Stat2Discord]"+ChatColor.RED+" An SQL error occured:");
+            throwables.printStackTrace();
+        }
+
+        Discord.zplayer = new ArrayList<>();
+
         System.out.println(ChatColor.YELLOW+"[Stat2Discord] Enabling Discord asynchronously...");
         BukkitRunnable r = new BukkitRunnable() {
             @Override
@@ -40,11 +90,26 @@ public class SpigotEvent extends JavaPlugin{
             }
         };
         r.runTaskAsynchronously(this);
-        System.out.println(ChatColor.YELLOW+"[Stat2Discord] Found statistic data of "+ counter +" players.");
     }
 
     @Override
     public void onDisable() {
         System.out.println(ChatColor.YELLOW+"[Stat2Discord] v0.1 disabling plugin");
+    }
+
+    //connect to MySQL database safely
+    private void openConnection() throws SQLException, ClassNotFoundException {
+        if (connection != null && !connection.isClosed()) {
+            return;
+        }
+
+        synchronized (this) {
+            if (connection != null && !connection.isClosed()) {
+                return;
+            }
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://" + fc.getString("hostname")+ ":" + fc.getInt("port") +
+                    "/" + fc.getString("database"), fc.getString("username"), fc.getString("password"));
+        }
     }
 }
--- src/main/java/zerrium/ZPlayer.java
+++ src/main/java/zerrium/ZPlayer.java
@@ -8,8 +8,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.apache.commons.io.FileUtils;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 
 public class ZPlayer {
@@ -19,6 +21,7 @@ public class ZPlayer {
             crouch = 0L, boat = 0L, elytra = 0L, trade = 0L, talk = 0L, chest = 0L, fishing = 0L, enchant = 0L,
             slept = 0L, craft_kind = 0L, place_kind = 0L, mine_kind = 0L, pickaxe = 0L, axe = 0L, shovel = 0L,
             bow = 0L, sword = 0L, trident = 0L, mob_kind = 0L, slain_kind = 0L, craftn = 0L, minen = 0L, placen = 0L, hoe = 0L;
+    HashMap<String, Long> x = new HashMap<>(); //convert those stupid many attributes into a hasmap
     LinkedHashMap<Material, Long> craft = new LinkedHashMap<>();
     LinkedHashMap<Material, Long> place = new LinkedHashMap<>();
     LinkedHashMap<Material, Long> mine = new LinkedHashMap<>();
@@ -168,7 +171,135 @@ public class ZPlayer {
         BukkitRunnable r = new BukkitRunnable() {
             @Override
             public void run() {
+                try {
+                    PreparedStatement pss = SpigotEvent.connection.prepareStatement("select * from stats where uuid=?");
+                    pss.setString(1, uuid.toString());
+                    ResultSet rs = pss.executeQuery();
+                    pss.close();
+                    PreparedStatement ps;
+                    if(!rs.next()){
+                        ps = SpigotEvent.connection.prepareStatement("insert into stats(uuid, stat, val) values" +
+                                "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," +
+                                "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," +
+                                "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," +
+                                "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," +
+                                "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," +
+                                "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?)," + "(?, ?, ?);");
+                        for(int i=1; i<=138; i+=3){
+                            ps.setString(i, uuid.toString());
+                        }
+                        ps.setString(2, Statistic.PLAY_ONE_MINUTE.toString());
+                        ps.setLong(3, time_played);
+                        ps.setString(5, Statistic.DAMAGE_DEALT.toString());
+                        ps.setLong(6, dmg_dealt);
+                        ps.setString(8, Statistic.DAMAGE_TAKEN.toString());
+                        ps.setLong(9, dmg_taken);
+                        ps.setString(11, Statistic.MOB_KILLS.toString());
+                        ps.setLong(12, mob_kills);
+                        ps.setString(14, Statistic.DEATHS.toString());
+                        ps.setLong(15, deaths);
+                        ps.setString(17, Statistic.SPRINT_ONE_CM.toString());
+                        ps.setLong(18, sprint);
+                        ps.setString(20, Statistic.WALK_ONE_CM.toString());
+                        ps.setLong(21, walk);
+                        ps.setString(23, Statistic.CROUCH_ONE_CM.toString());
+                        ps.setLong(24, crouch);
+                        ps.setString(26, Statistic.BOAT_ONE_CM.toString());
+                        ps.setLong(27, boat);
+                        ps.setString(29, Statistic.AVIATE_ONE_CM.toString());
+                        ps.setLong(30, elytra);
+                        ps.setString(32, Statistic.TRADED_WITH_VILLAGER.toString());
+                        ps.setLong(33, trade);
+                        ps.setString(35, Statistic.TALKED_TO_VILLAGER.toString());
+                        ps.setLong(36, talk);
+                        ps.setString(38, Statistic.CHEST_OPENED.toString());
+                        ps.setLong(39, chest);
+                        ps.setString(41, Statistic.FISH_CAUGHT.toString());
+                        ps.setLong(42, fishing);
+                        ps.setString(44, Statistic.ITEM_ENCHANTED.toString());
+                        ps.setLong(45, enchant);
+                        ps.setString(47, Statistic.SLEEP_IN_BED.toString());
+                        ps.setLong(48, slept);
+                        ps.setString(50, Statistic.CRAFT_ITEM.toString());
+                        ps.setLong(51, craftn);
+                        ps.setString(53, Statistic.MINE_BLOCK.toString());
+                        ps.setLong(54, minen);
+                        ps.setString(56, "pickaxe");
+                        ps.setLong(57, pickaxe);
+                        ps.setString(59, "axe");
+                        ps.setLong(60, axe);
+                        ps.setString(62, "shovel");
+                        ps.setLong(63, shovel);
+                        ps.setString(65, "hoe");
+                        ps.setLong(66, hoe);
+                        ps.setString(68, "sword");
+                        ps.setLong(69, sword);
+                        ps.setString(71, "bow");
+                        ps.setLong(72, bow);
+                        ps.setString(74, "trident");
+                        ps.setLong(75, trident);
+                        ps.setString(77, "placed");
+                        ps.setLong(78, placen);
+                        ps.setString(80, "craft_kind");
+                        ps.setLong(81, craft_kind);
+                        ps.setString(83, "mine_kind");
+                        ps.setLong(84, mine_kind);
+                        ps.setString(86, "place_kind");
+                        ps.setLong(87, place_kind);
+                        ps.setString(89, "mob_kind");
+                        ps.setLong(90, mob_kind);
+                        ps.setString(92, "slain_kind");
+                        ps.setLong(93, slain_kind);
 
+                        Object cr[] = craft.keySet().toArray();
+                        ps.setString(95, "craft_1_"+cr[0].toString());
+                        ps.setLong(96, craft.get((Material) cr[0]));
+                        ps.setString(98, "craft_2_"+cr[1].toString());
+                        ps.setLong(99, craft.get((Material) cr[1]));
+                        ps.setString(101, "craft_3_"+cr[2].toString());
+                        ps.setLong(102, craft.get((Material) cr[2]));
+
+                        Object pl[] = place.keySet().toArray();
+                        ps.setString(104, "place_1_"+pl[0].toString());
+                        ps.setLong(105, place.get((Material) pl[0]));
+                        ps.setString(107, "place_2_"+pl[1].toString());
+                        ps.setLong(108, place.get((Material) pl[1]));
+                        ps.setString(110, "place_3_"+pl[2].toString());
+                        ps.setLong(111, place.get((Material) pl[2]));
+
+                        Object mn[] = mine.keySet().toArray();
+                        ps.setString(113, "mine_1_"+mn[0].toString());
+                        ps.setLong(114, mine.get((Material) mn[0]));
+                        ps.setString(116, "mine_2_"+mn[1].toString());
+                        ps.setLong(117, mine.get((Material) mn[1]));
+                        ps.setString(119, "mine_3_"+mn[2].toString());
+                        ps.setLong(120, mine.get((Material) mn[2]));
+
+                        Object mo[] = mob.keySet().toArray();
+                        ps.setString(122, "mob_1_"+mo[0].toString());
+                        ps.setLong(123, mob.get((EntityType) mo[0]));
+                        ps.setString(125, "mob_2_"+mo[1].toString());
+                        ps.setLong(126, mob.get((EntityType) mo[1]));
+                        ps.setString(128, "mob_3_"+mo[2].toString());
+                        ps.setLong(129, mob.get((EntityType) mo[2]));
+
+                        Object sl[] = slain.keySet().toArray();
+                        ps.setString(131, "slain_1_"+sl[0].toString());
+                        ps.setLong(132, slain.get((EntityType) sl[0]));
+                        ps.setString(134, "slain_2_"+sl[1].toString());
+                        ps.setLong(135, slain.get((EntityType) sl[1]));
+                        ps.setString(137, "slain_3_"+sl[2].toString());
+                        ps.setLong(138, slain.get((EntityType) sl[2]));
+
+                        ps.executeUpdate();
+                        ps.close();
+                    }else{
+                        ps = SpigotEvent.connection.prepareStatement("update stats set ");
+                    }
+                    rs.close();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
             }
         };
 
